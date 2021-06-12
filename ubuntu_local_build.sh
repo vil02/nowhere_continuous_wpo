175a1d0b3d8ae388566f5057f6f147930972687c
Piotr Idzik <vil02@o2.pl>
2021-06-12 21:50:49 +0200

    ubuntu_local_build.sh added

---
+++ ubuntu_local_build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+readonly FILE_NAME="nowhere_continuous_wpo"
+readonly FILE_EXT=".tex"
+pdflatex -interaction=batchmode -draftmode "$FILE_NAME$FILE_EXT"
+bibtex $FILE_NAME
+pdflatex -interaction=batchmode -draftmode "$FILE_NAME$FILE_EXT"
+pdflatex -interaction=batchmode "$FILE_NAME$FILE_EXT"
